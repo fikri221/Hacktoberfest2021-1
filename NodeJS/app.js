ba1cbcdbca7b0d4e5fc259b10937082ff8d47542
Fikri Lazuardi <56496088+fikri221@users.noreply.github.com>
2021-10-22 18:04:10 +0700

    my-recipes
    Back-end REST API to provide any food recipes.

---
+++ NodeJS/app.js
@@ -0,0 +1,83 @@
+// NPM modules
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let session = require('express-session');
+let FileStore = require('session-file-store')(session);
+let passport = require('passport');
+let authenticate = require('./authenticate');
+const mongoose = require('mongoose');
+let config = require('./config');
+
+// Router modules
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+let dishRouter = require('./routes/dishRouter');
+let promoRouter = require('./routes/promoRouter');
+let leaderRouter = require('./routes/leaderRouter');
+let uploadRouter = require('./routes/uploadRouter');
+let favoriteRouter = require('./routes/favoriteRouter');
+
+// Set up connection to mongodb
+const url = config.mongoUrl;
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log('Connected correctly to the server');
+}, (err) => {
+  console.log(err);
+})
+
+let app = express();
+
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Middlewares
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(passport.initialize());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+app.use('/favorites', favoriteRouter);
+app.use('/imageUpload', uploadRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
